596b72f49643cd0b165460e300ddb0a38a594c68
Mike <zluo@alumni.berklee.edu>
2018-02-09 18:42:07 -0500

    Building loggin modal

--- src/components/homeContainer.js
+++ src/components/homeContainer.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import ChatContainer from './chatComponents/chatContainer'
+import Modal from 'react-modal'
 import { ConnectedUsersList } from './usersComponents/usersList'
 import { ConnectedUserProfile } from './profileComponents/UserProfile'
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 
 export default class HomeContainer extends Component {
 
@@ -10,6 +23,13 @@ export default class HomeContainer extends Component {
   render() {
     return (
       <div className="wrapper">
+        <Modal
+          isOpen={true}
+          style={customStyles}
+        >
+          <h2>Modal Here!</h2>
+        </Modal>
+
         <ConnectedUsersList />
         <ChatContainer />
         <ConnectedUserProfile />
